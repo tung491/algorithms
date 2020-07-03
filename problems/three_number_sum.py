8540858ba9419e3077401ab2812a834b78e217a2
tung491 <dosontung007@gmail.com>
2020-07-03 20:30:34 +0700

    Add three_number_sum.py

--- problems/three_number_sum.py
+++ problems/three_number_sum.py
@@ -1,13 +1,13 @@
 def three_number_sum(input_list: list, target: int) -> list:
     triplets = []
-    input_list.sort()
-    for i, n in enumerate(input_list[:-2]):
+    nums.sort()
+    for i, n in enumerate(nums[:-2]):
         left = i + 1
-        right = len(input_list) - 1
+        right = len(nums) - 1
         while left < right:
-            curr_sum = n + input_list[left] + input_list[right]
+            curr_sum = n + nums[left] + nums[right]
             if curr_sum == target:
-                triplets.append([n, input_list[left], input_list[right]])
+                triplets.append([n, nums[left], nums[right]])
                 left += 1
                 right -= 1
             elif curr_sum < target:
