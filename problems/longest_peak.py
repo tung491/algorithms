f05c86a4929f2790f9c0d26e7384301886ba8d78
tung491 <dosontung007@gmail.com>
2020-07-11 12:35:57 +0700

    Improve logic of travesal

--- problems/longest_peak.py
+++ problems/longest_peak.py
@@ -1,20 +1,25 @@
 def longest_peak(arr):
     longest_peak_len = 0
     i = 1
+    left_i = 0
     while i < len(arr) - 1:
-        is_peak = max(arr[i], arr[i - 1], arr[i + 1]) == arr[i]
+        is_peak = arr[i] > arr[i - 1] and arr[i] > arr[i + 1]
         if not is_peak:
+            if arr[i] <= arr[i - 1]:
+                left_i = i
             i += 1
             continue
 
-        left_i = i - 2
-        while left_i >= 0 and arr[left_i] < arr[left_i + 1]:
-            left_i -= 1
         right_i = i + 2
         while right_i < len(arr) and arr[right_i] < arr[right_i - 1]:
             right_i += 1
         
-        curr_peak_len = right_i - left_i - 1
+        curr_peak_len = right_i - left_i
         longest_peak_len = max(longest_peak_len, curr_peak_len)
-        i = right_i
+        i = right_i - 1
     return longest_peak_len
+
+
+if __name__ == "__main__":
+    print(longest_peak([1, 2, 3, 3, 4, 0, 10,
+    6, 5, -1, -3, 2, 3]))
